8714d4bc5268f76f183822f21ea1128bf7d11eba
EkaterinA <azarova_2212@mail.ru>
2022-03-19 10:56:07 +0300

    commit

--- main.c
+++ main.c
@@ -33,6 +33,7 @@ void selectionSort(int *a, size_t size) {
         swap(&a[i], &a[minPos]);
     }
 }
+
 // Выполняет сортировку вставками
 void insertionSort(int *a, const size_t size) {
     for (size_t i = 1; i < size; i++) {
@@ -83,27 +84,27 @@ int digit(int n, int k, int N, int M) {
     return (n >> (N * k) & (M - 1));
 }
 
-void _radixSort(int *l, int *r, int N) {
+void _radixSort(int *left, int *right, int N) {
     int k = (32 + N - 1) / N;
     int M = 1 << N;
-    int sz = r - l;
+    int sz = right - left;
     int *b = (int *) malloc(sizeof(int) * sz);
     int *c = (int *) malloc(sizeof(int) * M);
     for (int i = 0; i < k; i++) {
         for (int j = 0; j < M; j++)
             c[j] = 0;
 
-        for (int *j = l; j < r; j++)
+        for (int *j = left; j < right; j++)
             c[digit(*j, i, N, M)]++;
 
         for (int j = 1; j < M; j++)
             c[j] += c[j - 1];
 
-        for (int *j = r - 1; j >= l; j--)
+        for (int *j = right - 1; j >= left; j--)
             b[--c[digit(*j, i, N, M)]] = *j;
 
         int cur = 0;
-        for (int *j = l; j < r; j++)
+        for (int *j = left; j < right; j++)
             *j = b[cur++];
     }
     free(b);
@@ -153,7 +154,7 @@ void checkTime(void (*sortFunc )(int *, size_t),
     static size_t runCounter = 1;
 
     // генерация последовательности
-    static int innerBuffer[100000];
+    static int innerBuffer[500000];
     generateFunc(innerBuffer, size);
     printf("Run #%zu| ", runCounter++);
     printf(" Name : %s\n", experimentName);
@@ -171,7 +172,7 @@ void checkTime(void (*sortFunc )(int *, size_t),
 
         // запись в файл
         char filename[256];
-        sprintf(filename, "./data/%s.csv", experimentName);
+        sprintf(filename, "./time/%s.csv", experimentName);
         FILE *f = fopen(filename, "a");
         if (f == NULL) {
             printf(" FileOpenError %s", filename);
@@ -216,11 +217,6 @@ void generateOrderedBackwards(int *a, size_t size) {
 void timeExperiment() {
     // описание функций сортировки
     SortFunc sorts[] = {
-            {selectionSort, "selectionSort"},
-            {insertionSort, "insertionSort"},
-            {bubbleSort,    "bubbleSort"},
-            {combSort,      "combSort"},
-            {ShellSort,     " ShellSort"},
             {radix_Sort,    "radix_Sort"},
     };
     const unsigned FUNCS_N = ARRAY_SIZE(sorts);
@@ -237,7 +233,7 @@ void timeExperiment() {
     const unsigned CASES_N = ARRAY_SIZE(generatingFuncs);
 
     // запись статистики в файл
-    for (size_t size = 10000; size <= 100000; size += 10000) {
+    for (size_t size = 100000; size <= 200000; size += 10000) {
         printf(" - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
         printf("Size : %d\n", size);
         for (int i = 0; i < FUNCS_N; i++) {
